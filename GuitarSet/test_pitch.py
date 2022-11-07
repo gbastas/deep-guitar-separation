27adc222ea5fa1b99955c2a624bd1694b0984dea
Grigoris Bastas <greg.bastas@gmail.com>
2022-11-07 19:45:02 +0200

    changes

--- GuitarSet/test_pitch.py
+++ GuitarSet/test_pitch.py
@@ -15,6 +15,7 @@ from matplotlib import lines as mlines, pyplot as plt
 import librosa.display
 
 from scipy.io import wavfile
+from tensorflow.keras.utils import to_categorical
 
 import sys
 np.set_printoptions(threshold=sys.maxsize)
@@ -66,7 +67,8 @@ class TrackInstance():
         self.predicted_tablature = None
         self.rnn_tablature = None
         self.predicted_strings = None
-
+        self.highest_fret = 19
+        self.num_classes = self.highest_fret + 2
 
     def predict_tablature(self, mode = 'FromCNN'):
         '''mode --> {From_Annos, FromCNN} first reads 
@@ -107,35 +109,50 @@ class TrackInstance():
 
         return Tablature(onsets, midi_notes, strings)
     
-    def predict_onsets(self):
-        proc_0 = CNNOnsetProcessor()
-        proc_1 = OnsetPeakPickingProcessor(threshold = 0.95,fps=100)
-        predicts = proc_1(proc_0(self.track_name))
-
-        #=====manually adding true onsets
-        #predicts = [onset.prediction for onset in self.true_tablature.onsets]
-        #====
-
-        return list(zip(predicts, [1]*len(predicts))) # here correct it when i can get confidence
-
 
     def predict_pyin(self): # __gbastas__
-        f0, voiced_flag, voiced_probs = librosa.pyin(self.audio, fmin=librosa.note_to_hz('E2'),fmax=librosa.note_to_hz('E6')) #hop_length=512
-        times = librosa.times_like(f0)  #hop_length=512
-        print('pyin f0 (raw)',len(f0))
-        print('pyin times',len(times))
+        f0 = [None,None,None,None,None,None]
+        # string_range
 
-        D = librosa.amplitude_to_db(np.abs(librosa.stft(x.audio)), ref=np.max) #hop_length=512
-        fig, ax = plt.subplots()
-        img = librosa.display.specshow(D, x_axis='time', y_axis='log', ax=ax)
-        ax.set(title='pYIN fundamental frequency estimation')
-        fig.colorbar(img, ax=ax, format="%+2.f dB")
-        ax.plot(times, f0, label='f0', color='cyan', linewidth=3)
-        ax.legend(loc='upper right')
-        plt.savefig('visual.png')   
+        for string_num in range(6):
+            string_range=[{'min':'E2', 'max':'B3'}, {'min':'A2', 'max':'E4'}, {'min':'D3', 'max':'A4'}, {'min':'G3', 'max':'D5'}, {'min':'B3', 'max':'G5'}, {'min':'E4', 'max':'B5'}]
+            f0[string_num], voiced_flag, voiced_prob = librosa.pyin(self.audio[string_num,:], fmin=librosa.note_to_hz('E2'),fmax=librosa.note_to_hz('E6'),sr=44100) # hop_length=512 TODO: change range according to string
+            # f0[string_num], _, _ = librosa.pyin(self.audio[string_num,:], fmin=librosa.note_to_hz(string_range[string_num]['min']),fmax=librosa.note_to_hz(string_range[string_num]['max']), sr=44100) # hop_length=512 TODO: change range according to string
+            times = librosa.times_like(f0[string_num], sr=44100)  #hop_length=512
+            print('pyin f0 (raw)',len(f0[string_num]))
+            print('pyin times',len(times))
+
+            D = librosa.amplitude_to_db(np.abs(librosa.stft(self.audio[string_num,:])), ref=np.max) #hop_length=512
+            fig, ax = plt.subplots()
+            img = librosa.display.specshow(D, x_axis='time', y_axis='log',  sr=44100, ax=ax)
+            ax.set(title='pYIN fundamental frequency estimation')
+            fig.colorbar(img, ax=ax, format="%+2.f dB")
+            ax.plot(times, f0[string_num], label='f0', color='cyan', linewidth=3)
+            ax.legend(loc='upper right')
+            plt.savefig('visual'+str(string_num)+'.png')   
         return f0, times
 
-    def load_rep_and_labels_from_raw_file(self, filename, f0): # __gbastas__ copied from tab-cnn repo
+
+    def categorical(self, label): # __gbastas__ copied from tab-cnn repo
+        return to_categorical(label, self.num_classes)
+
+    def correct_numbering(self, n): # __gbastas__ copied from tab-cnn repo
+        n += 1
+        if n < 0 or n > self.highest_fret:
+            n = 0
+        return n
+
+    def clean_label(self, label): # __gbastas__ copied from tab-cnn repo
+        label = [self.correct_numbering(n) for n in label]
+        return self.categorical(label)
+    
+    def clean_labels(self, labels): # __gbastas__ copied from tab-cnn repo
+        return np.array([self.clean_label(label) for label in labels])
+
+    def isNaN(self, num):
+        return num != num
+
+    def load_rep_and_labels_from_raw_file(self, filename, f0_pred): # __gbastas__ copied from tab-cnn repo and changed it a bit
         string_midi_pitches = [40,45,50,55,59,64]
         file_audio = self.track_name
         file_anno = self.jam_name
@@ -143,108 +160,53 @@ class TrackInstance():
         self.sr_original, data = wavfile.read(file_audio)
         self.sr_curr = self.sr_original
         
-        frame_indices = range(len(f0))
+        frame_indices = range(len(f0_pred[0]))
         times = librosa.frames_to_time(frame_indices, sr = self.sr_curr, hop_length=512)
-        # times = librosa.frames_to_time(frame_indices, sr = self.sr_curr, hop_length=512)
-        # print(jam.annotations["note_midi"][4]['data'].value)
-        # print(anno.to_samples(times))
 
-        # aaa
         # loop over all strings and sample annotations
         labels = []
-        f0_gt = np.array([-1]*len(f0)) #np.array(len(f0))
+        preds = []
         for string_num in range(6):
-            # anno = annotations
+            # Predictions
+            tmp = np.array([-1] * len(f0_pred[0]))
+            for i in frame_indices:
+                if not self.isNaN(f0_pred[string_num][i]):
+                    # replace midi pitch values with fret numbers
+                    tmp[i] = int(round(hz2midi(f0_pred[string_num][i])) - string_midi_pitches[string_num])
+                    if tmp[i]<0:
+                        tmp[i]=-1
+            preds.append([tmp])
+
+            # Annotations
             anno = jam.annotations["note_midi"][string_num]
             string_label_samples = anno.to_samples(times)
-            print(string_label_samples)
-            # aaa
-            # # replace midi pitch values with fret numbers
+            # replace midi pitch values with fret numbers
             for i in frame_indices:
                 if string_label_samples[i] == []:
                     string_label_samples[i] = -1
-                    # midi_gt.append(-1)
                 else:
-                    string_label_samples[i] = string_label_samples[i][0]
-                    # print(string_label_samples[i])
-                    # string_label_samples[i] = int(round(string_label_samples[i][0]) - string_midi_pitches[string_num])
-                    # print(round(string_label_samples[i]))
-                    f0_gt[i] = string_label_samples[i]
-
+                    string_label_samples[i] = int(round(string_label_samples[i][0]) - string_midi_pitches[string_num])
             labels.append([string_label_samples])
-
-        # midi_frames = librosa.samples_to_frames(string_label_samples)
-
-            # print(string_label_samples) 
-        # print(string_label_samples[:10])
-        # midi_gt_f = librosa.samples_to_frames(midi_gt)
-        # print(len(midi_gt_f))
-        # print(string_labe_samples.flatten)
-
-        # print(np.count_nonzero(string_label_samples != []))
-        # aaa
-
+            
         labels = np.array(labels)
-        # # remove the extra dimension 
         labels = np.squeeze(labels)
         labels = np.swapaxes(labels,0,1)
         
-        # print(f0_gt)
-        # print(labels.shape)
+        # clean labels
+        labels = self.clean_labels(labels)
+        print('labels',labels.shape)
 
-        # aaa
-        # # clean labels
-        # labels = self.clean_labels(labels) # NOTE: to avoid wrong dataset annotations
+        preds = np.array(preds)
+        preds = np.squeeze(preds)
+        preds = np.swapaxes(preds,0,1)
+
+        # print(preds)
+
+        preds = self.clean_labels(preds) # NOTE: maybe not good
         
-        # # store and return
-        # labels
+        print('preds', preds.shape)
 
-        return f0_gt
-
-
-
-    def predict_notes_at_time(self,onsets):
-        midi_notes = []
-
-        local_onsets = list(zip(*onsets))[0]
-        time, frequency, confidence, activation = crepe.predict(self.audio, self.sr, viterbi=True)
-        time = list(time)
-        frequency = list(frequency)
-        confidence = list(confidence)
-        for x in local_onsets:
-            ind = time.index(round(x,2))
-            f_ind = ind # check to get better predictions
-            max_so_far = 0
-            if ind != 0 and ind<len(local_onsets)-3:
-                for p in range(ind-1,ind+2): # maybe can fix fundumental here, and not just round it
-                    if confidence[p]>max_so_far:
-                        f_ind = p
-                        max_so_far = confidence[p]
-            midi_notes.append((round(hz2midi(frequency[f_ind])), confidence[f_ind]))
-        return midi_notes
-
-    def predict_notes_onsets(self):
-        onsets = self.predict_onsets()
-        midi_notes = self.predict_notes_at_time(onsets)
-        # print(onsets, midi_notes)
-        # print(midi_notes)
-        return onsets, midi_notes
-
-    
-    def get_accuracy_of_prediction(self, mode = 'FromAnnos'):
-        tab = self.temp_tablature # __now__
-
-        pre_on = [onset.prediction for onset in tab.onsets]
-        # pre_on = [onset.prediction for (onset, t_midi, p_midi) in zip(tab.onsets,  self.true_tablature.midi_notes, tab.midi_notes) if p_midi.prediction==t_midi.prediction ] # __gbastas__
-        tru_on = [onset.prediction for onset in self.true_tablature.onsets]
-        tp, fp, tn, fn, errors = onset_evaluation(pre_on, tru_on, window = 0.025)
-
-        recall = len(tp)/(len(tp)+len(fn))
-        precision = len(tp)/(len(tp)+len(fp))
-        f1_measure = 2*recall*precision/(recall+precision)
-        print()
-        print('Onsets Recall is {} and Precision is {} and F1_measure is {}'.format(recall, precision, f1_measure))
-        print()
+        return labels, preds
 
 
 class Predictions():
@@ -277,9 +239,23 @@ class Tablature():
         self.strings = [String(x) for x in strings]
     
 
+def tab2pitch(tab):
+    pitch_vector = np.zeros(44)
+    string_pitches = [40, 45, 50, 55, 59, 64]
+    for string_num in range(len(tab)):
+        fret_vector = tab[string_num]
+        fret_class = np.argmax(fret_vector, -1)
+        # 0 means that the string is closed 
+        if fret_class > 0:
+            pitch_num = fret_class + string_pitches[string_num] - 41
+            pitch_vector[pitch_num] = 1
+    return pitch_vector
+
+
 if __name__ == '__main__':
 
-    dataset = 'mix'
+
+    dataset = 'hex_cln'
     workspace = initialize_workspace('./data/')
     annotations_folder = 'data/annos'
     jam_name = annotations_folder+'/05_BN1-147-Gb_solo.jams'
@@ -287,23 +263,12 @@ if __name__ == '__main__':
     x = TrackInstance(jam_name, dataset)
 
     f0_pred, times = x.predict_pyin()
-    f0_gt = x.load_rep_and_labels_from_raw_file(jam_name, f0_pred)
+    tab_gt, tab_pred = x.load_rep_and_labels_from_raw_file(jam_name, f0_pred)
  
-    # print([midi_note.prediction for midi_note in x.true_tablature.midi_notes])
-
-    # aaa
-
-    print(type(f0_gt))
-    print(f0_gt.shape)
-
-    print(type(f0_pred))
-    print(f0_pred.shape)
-
-    print(f0_gt[100:180])
-    print(f0_pred[100:180])
-
-    print(pitch_precision(f0_pred, f0_gt))
+    print(pitch_precision(tab_pred, tab_gt))
     
+
+
     # x.predict_tablature()
     # x.get_accuracy_of_prediction('FromCNN')
 
